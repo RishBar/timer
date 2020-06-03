63c6c32694d0a8dd307b9214a7521dd8e8d2a189
Rishav Baral <rishavbaral6@gmail.com>
2020-06-03 02:02:44 +0000

    add simple timer

--- timer1.js
+++ timer1.js
@@ -1,7 +1,12 @@
-for (const y of process.argv.slice(2)) {
+const newArr = process.argv.slice(2).sort();
+
+for (let y of newArr) {
+  y = Number(y);
   console.log(y);
+  if (y < 0 || isNaN(y)) {
+    return;
+  }
   setTimeout(() => {
     process.stdout.write('\x07');
-  }, y);
+  }, y * 1000);
 }
-
